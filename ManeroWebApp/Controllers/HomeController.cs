5a72a7a6e49e1afa797d0146aad6142f1cab612a
itishadi <113441103+itishadi@users.noreply.github.com>
2023-11-01 14:17:58 +0100

    Featured Products, har byggt grid och product card, samt javascript fil som fechar api

--- ManeroWebApp/Controllers/HomeController.cs
+++ ManeroWebApp/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace ManeroWebApp.Controllers
         {
             return View();
         }
+        public IActionResult FeaturedProduct()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
