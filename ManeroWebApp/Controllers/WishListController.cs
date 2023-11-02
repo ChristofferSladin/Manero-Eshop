d272acaca33987532f86d764aabd311f90fe3b97
gmiqbalian <gmiqbalian@gmail.com>
2023-11-02 21:10:31 +0100

    -WishList controller created -FavoriteProductViewModel created -FavoriteProductCardPartial created -WishList view created -Css defined for all front-end views and partials

---
+++ ManeroWebApp/Controllers/WishListController.cs
@@ -0,0 +1,65 @@
+﻿using DataAccessLibrary.Contexts;
+using DataAccessLibrary.Entities.ProductEntities;
+using DataAccessLibrary.Entities.UserEntities;
+using ManeroWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManeroWebApp.Controllers
+{
+    public class WishListController : Controller
+    {
+        private readonly ManeroDbContext _context;
+        private readonly HttpClient _httpClient;
+        public WishListController(ManeroDbContext context)
+        {
+            _context = context;
+            _httpClient = new HttpClient();
+        }
+        public IActionResult WishList(string forUserId = "6c0d8713-a32a-4209-a79b-6744ca401197") //user id hard coded for testing
+        {
+            var wishList = _context.Favorite.Include(fp => fp.FavoriteProducts)!
+                .ThenInclude(p => p.Product)
+                .FirstOrDefault(f => f.Id == forUserId);
+
+            if (wishList is not null)
+            {
+                var favProductList = wishList.FavoriteProducts!
+                    .Select(x => new FavoriteProductViewModel
+                    {
+                        UserId = forUserId,
+                        ProductId = x.Product.ProductId,
+                        ImgUrl = x.Product.ImageUrl,
+                        Name = x.Product.ProductName,
+                        Price = x.Product.SalePrice,
+                        PriceWithTax = x.Product.PriceIncTax,
+                        PriceWithoutTax = x.Product.PriceExcTax,
+                        Rating = x.Product.Rating,
+                        IsOnSale = x.Product.IsOnSale,
+                        ShoppingCartId = _context.ShoppingCarts
+                        .FirstOrDefault(x => x.Id == forUserId)!.ShoppingCartId,
+                    }).ToList();
+
+                if (favProductList is not null)
+                    return View(favProductList);
+            }
+
+            return View();
+        }
+        public async Task<IActionResult> AddProductToShoppingCart(int productId, int shoppingCartId, decimal priceWithTax, decimal priceWithoutTax)
+        {
+            await _context.ShoppingCartProducts
+                .AddAsync(new ShoppingCartProduct
+                {
+                    TotalPriceExcTax = priceWithoutTax,
+                    TotalPriceIncTax = priceWithTax,
+                    ProductId = productId,
+                    ShoppingCartId = shoppingCartId,
+                    ItemQuantity = 1,
+                });
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("WishList");
+        }
+    }
+}
---
+++ ManeroWebApp/Models/FavoriteProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Policy;
+
+namespace ManeroWebApp.Models
+{
+    public class FavoriteProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string UserId {  get; set; }
+        public int ShoppingCartId { get; set; }
+        public string? ImgUrl { get; set; }
+        public string? Name {  get; set; }
+        public decimal PriceWithTax { get; set; }
+        public decimal PriceWithoutTax { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? Rating { get; set; }
+        public bool? IsOnSale { get; set; }
+    }
+}
