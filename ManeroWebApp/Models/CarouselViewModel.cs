3897bd0861e369e546fe7b95677d3fa4f62bb714
Christoffer Sladin <christoffer.sladin@hotmail.com>
2023-11-06 13:11:08 +0100

    -carousel and product partials work homecontroller changed, added functionality fixed javascript in home/index

--- ManeroWebApp/Models/CarouselViewModel.cs
+++ ManeroWebApp/Models/CarouselViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace ManeroWebApp.Models
+﻿using ServiceLibrary.Models;
+
+namespace ManeroWebApp.Models
 {
     public class CarouselViewModel
     {
-        public string IdSuffix { get; set; }
-        public string Title { get; set; }
+        public string? IdSuffix { get; set; }
+        public string? Title { get; set; }
+        public List<Product> Products { get; set; } = null!;
+        public List<Product> ProductsOnSale { get; set; } = null!;
+        public List<Product> FeaturedProducts { get; set; } = null!;
     }
 }
