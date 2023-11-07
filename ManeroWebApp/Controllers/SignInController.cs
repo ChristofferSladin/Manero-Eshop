693bd05af004e1af287e31c077aba00f7f49d8b2
AlexisBriceno98 <alex00534@hotmail.com>
2023-11-07 10:21:18 +0100

    WelcomePage is done for now.

---
+++ ManeroWebApp/Controllers/SignInController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManeroWebApp.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ ManeroWebApp/Controllers/WelcomePageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManeroWebApp.Controllers
+{
+    public class WelcomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
