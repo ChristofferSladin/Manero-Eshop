bfd6f93768f38f3d192b3ed015456c628eccf309
itishadi <113441103+itishadi@users.noreply.github.com>
2023-11-08 12:36:12 +0100

    Chenged controller

--- ManeroWebApp/Controllers/ProductController.cs
+++ ManeroWebApp/Controllers/ProductController.cs
@@ -16,45 +16,10 @@ namespace ManeroWebApp.Controllers
             _productService = productService;
             _userService = userService;
         }
-        public IActionResult FeaturedProduct()
+        public IActionResult FeaturedProducts()
         {
             return View();
         }
-        public async Task<IActionResult> AddProductToWishList(int productId)
-        {
-            var result = await _userService.AddProductToWishList(productId, "daf64035-b7fb-4bd1-b863-e19cab55699f");
-            if(result!=null)
-            {
-                TempData["success"] = $"Product added to wish list.";
-                return RedirectToAction("FeaturedProduct", "Product");
-            }
-            TempData["error"] = $"Somthing went wrong.";
-            return RedirectToAction("FeaturedProduct", "Product");
-        }
-        public async Task<IActionResult> ProductCardsPartial()
-        {
-            var products = await _productService.GetProductsWithReviewsAsync();
-            var productViewModels = products.Select(product => new ProductViewModel
-            {
-                ProductId = product.ProductId,
-                ProductNumber = product.ProductNumber,
-                ProductName = product.ProductName,
-                Description = product.Description,
-                Category = product.Category,
-                Type = product.Type,
-                Size = product.Size,
-                QuantityInStock = product.QuantityInStock,
-                Color = product.Color,
-                PriceExcTax = product.PriceExcTax,
-                PriceIncTax = product.PriceIncTax,
-                SalePrice = product.SalePrice,
-                IsOnSale = product.IsOnSale,
-                IsFeatured = product.IsFeatured,
-                Rating = product.Rating,
-                ImageUrl = product.ImageUrl
-            }).ToList();
-
-            return PartialView("/Views/Shared/Product/_ProductCards.cshtml", productViewModels);
-        }
+      
     }
 }
