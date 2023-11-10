39331aa33a9c2235f35996216b300e613ab9ec95
Jonathan.Donath.Lopez <Jonathan.Donath.Lopez@hotmail.com>
2023-11-10 09:59:03 +0100

    -cleaned up review -review rating -reaview count -review partial

---
+++ DataAccessLibrary/Repositories/ReviewRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLibrary.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using DataAccessLibrary.Entities.ProductEntities;
+
+namespace DataAccessLibrary.Repositories
+{
+    public class ReviewRepository
+    {
+        private readonly ManeroDbContext _context;
+        public ReviewRepository(ManeroDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Review>> GetFilteredReviewsAsync(int skip, int take, Expression<Func<Review, bool>> filterByName = null!, Expression<Func<Review, dynamic>> orderByField = null!, string orderDirection = null!)
+        {
+            var query = _context.Reviews.AsQueryable();
+
+            if (filterByName != null!)
+            {
+                query = query.Where(filterByName);
+            }
+
+            if (orderByField != null!)
+            {
+                query = orderDirection switch
+                {
+                    "asc" => query.OrderBy(orderByField),
+                    "desc" => query.OrderByDescending(orderByField),
+                    _ => query
+                };
+            }
+
+            if (take == 0)
+            {
+                return await query.ToListAsync();
+            }
+
+            return await query.Skip(skip).Take(take).ToListAsync();
+        }
+    }
+}
--- ManeroWebApp/Models/ReviewViewModel.cs
+++ ManeroWebApp/Models/ReviewViewModel.cs
@@ -4,6 +4,7 @@
     {
         public int ReviewId { get; set; }
         public int Rating { get; set; }
+        public string? ProductName { get; set; }
         public DateTime Created { get; set; }
         public string Content { get; set; } = null!;
         public string Title { get; set; } = null!;
