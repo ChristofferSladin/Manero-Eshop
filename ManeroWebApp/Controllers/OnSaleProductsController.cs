6b55670275a8329ea2992fd0bf32c9e82767c2c3
Erim <erimby@hotmail.com>
2023-11-10 09:25:08 +0100

    Added OnSaleProductsController

---
+++ ManeroWebApp/Controllers/OnSaleProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManeroWebApp.Controllers
+{
+    public class OnSaleProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
