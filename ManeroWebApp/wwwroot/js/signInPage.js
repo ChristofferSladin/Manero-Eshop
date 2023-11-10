e4f94d739014a5a58c65ee79c384ef83b43efbb6
AlexisBriceno98 <alex00534@hotmail.com>
2023-11-10 11:24:24 +0100

    The design is almost done, starting to work on the backend for login.

---
+++ ManeroWebApp/wwwroot/js/signInPage.js
@@ -0,0 +1,14 @@
+﻿function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('password');
+    var toggleIcon = document.querySelector('.toggle-password i');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleIcon.classList.remove('fa-eye');
+        toggleIcon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        toggleIcon.classList.add('fa-eye');
+        toggleIcon.classList.remove('fa-eye-slash');
+    }
+}
