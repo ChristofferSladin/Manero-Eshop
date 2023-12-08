ad34ec0e42b91937fe0da68beaca7bc16ce67be5
gmiqbalian <gmiqbalian@gmail.com>
2023-12-08 18:14:47 +0100

    -integration test for promo code controller

--- ManeroWebApp.Tests/ManeroApp/Controllers/PromoCodesController.cs
+++ ManeroWebApp.Tests/ManeroApp/Controllers/PromoCodesController.cs
@@ -1,28 +1,31 @@
-﻿using ManeroWebApp.Tests.Helpers;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+﻿using ManeroWebApp.Controllers;
+using ManeroWebApp.Models;
+using ManeroWebApp.Tests.Helpers;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using ServiceLibrary.Models;
 using ServiceLibrary.Services;
 using System.Net.Http.Headers;
 using System.Text;
 using UserAPI.Models;
 
-namespace ManeroWebApp.Tests;
+namespace ManeroWebApp.Tests.ManeroApp.Controllers;
 
-public class Test : IClassFixture<CustomWebApplication>
+public class PromoCodesController : IClassFixture<CustomWebApplication>
 {
     private readonly IPromoCodeService _promoCodeService;
+    private readonly ManeroWebApp.Controllers.PromoCodesController _promoCodesController;
     private readonly HttpClient _httpClient;
-    public Test(CustomWebApplication customWeb)
+    public PromoCodesController(CustomWebApplication customWeb)
     {
         _httpClient = customWeb.CreateClient();
         _httpClient.BaseAddress = new Uri("https://localhost:7047/");
         _promoCodeService = new PromoCodeService(_httpClient);
+        _promoCodesController = new ManeroWebApp.Controllers.PromoCodesController(_promoCodeService);
     }
     [Fact]
-    public async Task Testing()
+    public async Task Index_Should_ReturnToIndexView_When_PromoCodesListIsNotEmpty()
     {
+        //Arrange
         var user = new { email = "customer1@customer.com", password = "Customer123#" };
         var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
         var loginResponse = await _httpClient.PostAsync("/login", content);
@@ -30,10 +33,11 @@ public class Test : IClassFixture<CustomWebApplication>
         var token = JsonConvert.DeserializeObject<LoginResponse>(responseBody)!.AccessToken;
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
 
-        var result = await _promoCodeService.GetPromoCodesByUserAsync();
+        //Act
+        var result = await _promoCodesController.Index("current") as ViewResult;
 
+        //Assert
         Assert.NotNull(result);
-        Assert.IsType<List<PromoCode>>(result);
-
+        Assert.NotNull(result.Model);
     }
 }
